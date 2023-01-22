3f33af583cc2be8e98b7afc1750a4c868a5cc05a
odjhey <odjhey@gmail.com>
2023-01-22 11:05:16 +0800

    chore: add react query dev tools

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { trpc } from "./utils/trpc";
 import { httpBatchLink } from "@trpc/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import SomePage from "./pages/SomePage";
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         <SomePage></SomePage>
+        <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
       </QueryClientProvider>
     </trpc.Provider>
   );
