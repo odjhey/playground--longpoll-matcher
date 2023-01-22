1cc3f0bbe199fad3360106007554179ae4377475
odjhey <odjhey@gmail.com>
2023-01-22 11:21:48 +0800

    chore: add tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,5 @@
 import { Outlet, RouterProvider } from "react-router-dom";
 import { useState } from "react";
-import "./App.css";
 import { trpc } from "./utils/trpc";
 import { httpBatchLink } from "@trpc/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
--- web/src/components/Nav.tsx
+++ web/src/components/Nav.tsx
@@ -3,14 +3,18 @@ import { NavLink, Outlet } from "react-router-dom";
 export const Nav = () => {
   return (
     <>
-      <div>
+      <div className="py-1">
         <div>
-          <ul>
+          <ul className="menu menu-compact menu-horizontal gap-1">
             <li>
-              <NavLink to="/">Home</NavLink>
+              <NavLink to="/" className="p-0 px-1">
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/about">About</NavLink>
+              <NavLink to="/about" className="p-0 px-1">
+                About
+              </NavLink>
             </li>
           </ul>
         </div>
