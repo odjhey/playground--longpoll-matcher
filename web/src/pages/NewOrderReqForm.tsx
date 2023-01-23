af39b48ea44c88979ba3bee3fe60b0afef352122
odjhey <odjhey@gmail.com>
2023-01-24 06:23:58 +0800

    feat: update ui

--- web/src/pages/NewOrderReqForm.tsx
+++ web/src/pages/NewOrderReqForm.tsx
@@ -7,34 +7,40 @@ export default ({ afterSubmit }: { afterSubmit: () => void }) => {
 
   return (
     <>
-      <p className="text-lg">New Request</p>
-      <form
-        onSubmit={formMethods.handleSubmit((data) => {
-          newOrder
-            .mutateAsync({
-              ...data,
-            })
-            .then(() => {
-              afterSubmit();
-            });
-        })}
-      >
-        <input
-          type="text"
-          {...formMethods.register("by", { required: true })}
-          className="input input-sm"
-          placeholder="by"
-        ></input>
-        <input
-          type="text"
-          {...formMethods.register("details", { required: true })}
-          className="input input-sm"
-          placeholder="details"
-        ></input>
-        <button type="submit" className="btn btn-xs">
-          create
-        </button>
-      </form>
+      <div className="p-2 border-2 border-green-200">
+        <p className="text-lg">New Request</p>
+        <form
+          onSubmit={formMethods.handleSubmit((data) => {
+            newOrder
+              .mutateAsync({
+                ...data,
+              })
+              .then(() => {
+                afterSubmit();
+              });
+          })}
+        >
+          <div>
+            <div>
+              <input
+                type="text"
+                {...formMethods.register("by", { required: true })}
+                className="input input-sm"
+                placeholder="by"
+              ></input>
+              <input
+                type="text"
+                {...formMethods.register("details", { required: true })}
+                className="input input-sm"
+                placeholder="details"
+              ></input>
+            </div>
+            <button type="submit" className="btn btn-xs">
+              create
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
